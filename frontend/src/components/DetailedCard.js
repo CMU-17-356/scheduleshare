d7bb248584bd8c49a3f8850e54f3bc10a8a96c78
tlee0818 <texeirareborn@gmail.com>
2022-04-21 19:37:30 -0400

    hooks for detailed course and user info

--- frontend/src/components/DetailedCard.js
+++ frontend/src/components/DetailedCard.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const DetailedCard = ({id}) => {
+
+  const [course, setCourse] = useState({})
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "something",
+      body: {id: id}
+    }).then(res => {
+      setClasses(res.data)
+    }).catch(e => {
+    })
+  }, [])
+  
   return (
     <div>detailedCard</div>
   )
--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
-function UserInfo({user}) {
+function UserInfo({id}) {
+
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "something",
+      body: {id: id}
+    }).then(res => {
+      setUser(res.data)
+    }).catch(e => {
+    })
+  }, [])
+
   return (
     <div>UserInfo</div>
   )
