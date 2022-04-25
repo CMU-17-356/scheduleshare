ee5212e0b55afa8d2fe440e1b986502d0dac8f69
Max Sobkov <sobkovmax@gmail.com>
2022-04-25 00:55:32 -0400

    Changes to please the linter

--- backend/src/controllers/schedule_controllers.js
+++ backend/src/controllers/schedule_controllers.js
@@ -1,56 +1,55 @@
-const User = require('../schema/user_schema'); 
+const User = require('../schema/user_schema');
 
 const view_all_users = (req, res) => {
-    let users = User.find({}, (err, result) => {
-      if (err) {
-        res.status(400).send(err);
-      } else {
-        res.status(200).send(result);
-      }
+    const users = User.find({}, (err, result) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send(result);
+        }
     });
 };
 
 const view_user_by_id = (req, res) => {
-    let users = User.find({_id:req.params._id}, (err, result) => {
-      if (err) {
-        res.status(400).send(err);
-      } else {
-        res.status(200).send(result);
-      }
+    const users = User.find({_id: req.params._id}, (err, result) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send(result);
+        }
     });
 };
 
 
 const add_user = (req, res) => {
-    let user = new User(req.body).save((err, result) => {
-      if (err) {
-        res.status(400).send(err);
-      } else {
-        res.status(200).send("Successfully Added User with _id "+ result._id );
-      }
+    const user = new User(req.body).save((err, result) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send('Successfully Added User with _id '+ result._id );
+        }
     });
-  };
+};
 
 const update_user_by_id = (req, res) => {
-let user = User.updateOne({ _id: req.params._id}, req.body, (err, result) => {
-    if (err) {
-        res.status(400).send(err);
-    } else {
-        res.status(200).send("Successfully Updated User");
-    }
+    const user = User.updateOne({_id: req.params._id}, req.body, (err, result) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send('Successfully Updated User');
+        }
     });
 };
 
 const delete_user_by_id = (req, res) => {
-    let user = User.deleteOne({ _id: req.params._id}, (err, result) => {
+    const user = User.deleteOne({_id: req.params._id}, (err, result) => {
         if (err) {
             res.status(400).send(err);
         } else {
-            res.status(200).send("Successfully Deleted User");
+            res.status(200).send('Successfully Deleted User');
         }
-        });
-    };
-    
+    });
+};
 
 
 module.exports = {
@@ -58,5 +57,5 @@ module.exports = {
     view_user_by_id,
     add_user,
     update_user_by_id,
-    delete_user_by_id
-}
+    delete_user_by_id,
+};
