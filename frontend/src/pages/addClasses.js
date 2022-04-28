64cca7ddbbe69cf0982aa7308ecfd829f3a4c0e1
khushi02 <k9wadhwa@gmail.com>
2022-04-27 23:14:01 -0400

    UI Updates

--- frontend/src/pages/addClasses.js
+++ frontend/src/pages/addClasses.js
@@ -1,4 +1,5 @@
-// import React from 'react';
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
 import Header from "../components/Header";
 import SearchBar from "material-ui-search-bar";
 import {MuiThemeProvider} from "@material-ui/core/styles";
@@ -6,7 +7,7 @@ import useClasses from "../hooks/useClasses";
 import ScrollableList from "../components/ScrollableList"
 import Schedule from "../components/Schedule"
 import DetailedCard from "../components/DetailedCard"
-import {useState} from "react"
+// import {useState} from "react"
 
 function AddClassesPage() {
   //id should be this user's id
@@ -25,17 +26,15 @@ function AddClassesPage() {
   return (
     <div className='addClassesPage'>
       <Header />
-      <p>Add Classes Page!</p>
-      <MuiThemeProvider>
-        <div>
-          <SearchBar
-            style={{
-              margin: "0 auto",
-              maxWidth: 800
-            }}
-          />
-        </div>
-      </MuiThemeProvider>
+      <h2>Class Search</h2>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
       <ScrollableList myContents = {courses} isClass ={true} show = {show}/>
       {!expanded && <Schedule scheduleList = {[]}/>}
       {expanded && <DetailedCard id = {expandedID}/>}
--- frontend/src/pages/addFriends.js
+++ frontend/src/pages/addFriends.js
@@ -1,11 +1,12 @@
-// import React from 'react';
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
 import Header from "../components/Header";
 import SearchBar from "material-ui-search-bar";
 import {MuiThemeProvider} from "@material-ui/core/styles";
 import useUsers from "../hooks/useUsers";
 import ScrollableList from "../components/ScrollableList"
 import UserInfo from "../components/UserInfo"
-import { useState } from "react";
+// import { useState } from "react";
 
 function AddFriendsPage() {
   //id should be this user's id
@@ -21,17 +22,15 @@ function AddFriendsPage() {
   return (
     <div className='addFriendsPage'>
       <Header />
-      <p>Add Friends Page!</p>
-      <MuiThemeProvider>
-        <div>
-          <SearchBar
-            style={{
-              margin: "0 auto",
-              maxWidth: 800
-            }}
-          />
-        </div>
-      </MuiThemeProvider>
+      <h2>Friend Search</h2>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
       <ScrollableList myContents = {users} isClass ={false} show = {show}/>
       <UserInfo id = {expandedID}/>
     </div>
