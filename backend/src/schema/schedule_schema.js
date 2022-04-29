1d48761688890edf6142b526ee69e44e9ed236d2
Max Sobkov <sobkovmax@gmail.com>
2022-04-28 21:43:44 -0400

    Wrote and passed schedule tests

--- backend/src/schema/schedule_schema.js
+++ backend/src/schema/schedule_schema.js
@@ -1,8 +1,25 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema({
-    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    courses: [String],
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    },
+    courses: {
+        type: [String],
+        validate: {
+            validator(arr) {
+                if(arr.length == 0) return true;
+                arr.forEach(string => {
+                    if(/^\d{2}-\d{3}$/.test(string) == false) {
+                        throw new Error("array conatains an invalid course!")
+                    }
+                })
+                return true;
+            }
+        }
+    }
 });
 
 module.exports = mongoose.model('Schedule', scheduleSchema);
