c2741973c6ba7b6adf2bd7751c4078c154fcac41
Max Sobkov <sobkovmax@gmail.com>
2022-05-01 16:42:33 -0400

    Added tests for basic routes for User model

--- backend/src/controllers/user_controllers.js
+++ backend/src/controllers/user_controllers.js
@@ -1,44 +1,69 @@
 const User = require('../schema/user_schema');
+const {startDatabase} = require('../db/mongo');
+const mongoose = require('mongoose');
 
 const view_all_users = (req, res) => {
-    const users = User.find({}, (err, result) => {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.status(200).send(result);
-        }
+    const client = startDatabase();
+    client.connect(function(err, client){
+        const collection = client.db("ScheduleShare").collection('User');
+        collection.find({}).toArray(function(err, result) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).send(result);
+            }
+        });
     });
 };
 
 const view_user_by_id = (req, res) => {
-    const users = User.find({_id: req.params._id}, (err, result) => {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.status(200).send(result);
-        }
+    const client = startDatabase();
+    client.connect(function(err, client){
+        const collection = client.db("ScheduleShare").collection('User');
+        collection.find({_id: req.params._id}).toArray(function(err, result) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).send(result);
+            }
+        });
     });
 };
 
-
 const add_user = (req, res) => {
-    const user = new User(req.body).save((err, result) => {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.status(200).send('Successfully Added User with _id '+ result._id );
+    const client = startDatabase();
+    client.connect(function(err, client){
+        const collection = client.db("ScheduleShare").collection('User');
+        const user = new User(req.body)
+        try {
+            user.validate();
+            collection.insertOne(user, function(){
+                client.close();
+            });
+            res.status(200).send(user._id);
+        } catch(error) {
+            console.log(error);
+            res.status(400).send(error);       
         }
     });
 };
 
 const update_user_by_id = (req, res) => {
-    const user = User.updateOne({_id: req.params._id}, req.body, (err, result) => {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.status(200).send('Successfully Updated User');
-        }
-    });
+    const client = startDatabase();
+    client.connect(async function(err, client) {
+        const collection = client.db("ScheduleShare").collection('User');
+        console.log(req.params);
+        console.log(req.body);
+        const _id = mongoose.Types.ObjectId(req.params._id);
+        collection.updateOne({_id: _id}, {$set: req.body}, function (err, result) {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).send('Successfully Updated User');
+            }
+            client.close();
+        })
+    })
 };
 
 const delete_user_by_id = (req, res) => {
---
+++ backend/test/endpoints/user.test.js
@@ -0,0 +1,75 @@
+const {app} = require('../../src/server/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = require('chai').assert;
+const User = require('../../src/schema/user_schema');
+
+chai.use(chaiHttp);
+
+describe('Users (unparameterized)', function() {
+    it('should get all users from index route', function(done) {
+        const getRequest = chai.request(app).get('/user');
+        getRequest.end((err, res) => {
+            assert(res.statusCode==200);
+            done();
+        })
+    });
+
+    it('should POST a new user from add_user route', function(done) {
+        const data = 
+        {
+            'username':'mhilton',
+            'password':'IloveSE',
+            'full_name':'Michael Hilton'
+        }
+        const postRequest = chai.request(app).post('/user').send(data);
+        postRequest.end((err, res) => {
+            console.log(err);
+            assert(res.statusCode == 200);
+            done();
+        });
+    });
+})
+
+describe('Users (parameterized)', function() {
+    before(function(done) {
+        data = { 
+            'username':'a', 
+            'password':'alsoa', 
+            'full_name':'a a'
+        };
+        let user_id = "";
+        const postRequest = chai.request(app).post('/user').send(data);
+        postRequest.end((err, res) => {
+            console.log(err);
+            assert(res.statusCode == 200);
+            user_id = res.body
+            done();
+        });
+    })
+
+    it('should GET a user by their id', function(done) {
+        const getRequest = chai.request(app).get(`/user/${user_id}`);
+        getRequest.end((err, res) => {
+            assert(res.statusCode==200);
+        done();
+        })
+    })
+
+    // it('should DELETE a user by their id', function(done) {
+    //     const deleteRequest = chai.request(app).delete(`/user/${user_id}`);
+    //     deleteRequest.end((err, res) => {
+    //         assert(res.statusCode==200);
+    //     done();
+    //     })
+    // })
+
+    // //TODO add param to update
+    // it('should PUT revisions for a user by their id', function(done) {
+    //     const putRequest = chai.request(app).put(`/user/${user_id}`);
+    //     putRequest.end((err, res) => {
+    //         assert(res.statusCode==200);
+    //     done();
+    //     })
+    // })
+})
