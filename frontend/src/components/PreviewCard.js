1b9b276ac1766bcdbfd186c498581baca0dcf2d7
tlee0818 <texeirareborn@gmail.com>
2022-05-03 15:21:34 -0400

    added toast notifcation instead of alerts

--- frontend/src/components/PreviewCard.js
+++ frontend/src/components/PreviewCard.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,72 +6,85 @@ import CardActionArea from '@mui/material/CardActionArea';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import { borders } from '@mui/system';
+
+const PreviewCard = ({ content, isCourse, show, addContent }) => {
+
+  // const addClass = (courseName) => {
+
+  //   axios({
+  //     method: "PUT",
+  //     url: `http://localhost:3000/schedule/626f21bdf3743ba6b3ebf4d4/course/${content.course_id}`
+  //   }).then(res => {
+  //     toast.success('🦄 Wow so easy!', {
+  //       position: "top-right",
+  //       autoClose: 2,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //     });
+  //   }).catch(e => {
+  //     console.log(e)
+  //     alert("Error Adding Course")
+  //   })
+  // }
+
+  // const addFriend = () => {
+  //   axios({
+  //     method: "POST",
+  //     url: "http://localhost:3000/friend/",
+  //     body: {
+  //       friend_1: content.id,
+  //       friend_2: content.id
+  //     }
+  //   }).then(res => {
+  //     console.log(content.full_name, "added")
+  //     alert(`${content.full_name} is your friend!`)
+  //   }).catch(e => {
+  //     alert("Error Adding Friend")
+  //   })
+  // }
 
 
-const PreviewCard = ({ content, isCourse, show }) => {
-
-  const addClass = (courseName) => {
-
-    axios({
-      method: "PUT",
-      url: `http://localhost:3000/schedule/626f21bdf3743ba6b3ebf4d4/course/${content.course_id}`
-    }).then(res => {
-      alert(`${courseName} added to schedule!`)
-    }).catch(e => {
-      console.log(e)
-      alert("Error Adding Course")
-    })
-  }
-
-  const addFriend = () => {
-    axios({
-      method: "POST",
-      url: "http://localhost:3000/friend/",
-      body: {
-        friend_1: content.id,
-        friend_2: content.id
-        }        
-    }).then(res => {
-      console.log(content.full_name, "added")
-      alert(`${content.full_name} is your friend!`)
-    }).catch(e => {
-      alert("Error Adding Friend")
-    })
-  }
-
-  
   if (isCourse) {
     return (
-      <Card sx={{ minWidth: 275, boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-      backgroundColor: "#edfbff",}}>
-        <CardActionArea onClick={() => show(content)}>
+      <div>
+        <Card sx={{
+          minWidth: 275, boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+          backgroundColor: "#edfbff",
+        }}>
+          <CardActionArea onClick={() => show(content)}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {content.name}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+
+                {(content.course_id) + " " + (content.units) + " units"}
+
+              </Typography>
+            </CardContent>
+          </CardActionArea>
           <CardContent>
-            <Typography variant="h5" component="div">
-              {content.name}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-
-              {(content.course_id) + " " + (content.units) + " units"}
-
+            <Typography variant="text.primary">
+              {content.desc.substring(0, 90) + "..."}
             </Typography>
           </CardContent>
-        </CardActionArea>
-        <CardContent>
-          <Typography variant="text.primary">
-            {content.desc.substring(0,90) + "..." }
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={() => addClass(content.name)}>Add to schedule</Button>
-        </CardActions>
-      </Card>
+          <CardActions>
+            <Button size="small" onClick={() => addContent(content)}>Add to schedule</Button>
+          </CardActions>
+        </Card>
+      </div>
+
     )
   }
   else {
     return (
-      <Card sx={{ minWidth: 275, boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-      backgroundColor: "#edfbff",}}>
+      <Card sx={{
+        minWidth: 275, boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+        backgroundColor: "#edfbff",
+      }}>
         <CardActionArea onClick={() => show(content)}>
           <CardContent>
             <Typography variant="h5" component="div">
@@ -89,8 +101,8 @@ const PreviewCard = ({ content, isCourse, show }) => {
           </Typography>
         </CardContent>
         <CardActions>
-          {(!content.isFriend) && 
-          <Button size="small" onClick={() => addFriend()}>Add Friend</Button>
+          {(!content.isFriend) &&
+            <Button size="small" onClick={() => addContent(content)}>Add Friend</Button>
           }
         </CardActions>
       </Card>
--- frontend/src/components/ScrollableList.js
+++ frontend/src/components/ScrollableList.js
@@ -2,9 +2,9 @@ import PreviewCard from "./PreviewCard"
 import ReactScrollableList from 'react-scrollable-list'
 import { useState } from "react"
 
-const ScrollableList = ({ myContents, isCourse, show }) => {
+const ScrollableList = ({ myContents, isCourse, show, addContent }) => {
 
-  const listContents = myContents.map(myContent => ({ id: myContent.id, content: <PreviewCard content={myContent} isCourse={isCourse} show={show} /> }))
+  const listContents = myContents.map(myContent => ({ id: myContent.id, content: <PreviewCard content={myContent} isCourse={isCourse} show={show} addContent={addContent}/> }))
   
   return (
 
